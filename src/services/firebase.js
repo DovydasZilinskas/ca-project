1cc45182e8e144416e3202bc26a0186ac3114319
Dovydas Zilinskas <user@Dovydas-MacBook-Air.local>
2020-08-10 18:48:26 +0300

    firebase config failas ikeltas

---
+++ src/services/firebase.js
@@ -0,0 +1,15 @@
+/** @format */
+
+import firebase from "firebase/app";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAiPpFrHyFZIZX7Zi6fZdQPEesLWIWPdhE",
+  authDomain: "ca-project-studentai.firebaseapp.com",
+  databaseURL: "https://ca-project-studentai.firebaseio.com",
+  projectId: "ca-project-studentai",
+  storageBucket: "ca-project-studentai.appspot.com",
+  messagingSenderId: "648568767256",
+  appId: "1:648568767256:web:d218c9b55d9f7f20fa546d",
+};
+
+firebase.initializeApp(firebaseConfig);
