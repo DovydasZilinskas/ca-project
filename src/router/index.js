4cc4b0ff44d3476e945f2cfa106f0ca2ea2696fb
Dovydas Zilinskas <user@Dovydas-MacBook-Air.local>
2020-08-10 18:57:03 +0300

    prideta Login forma

--- src/router/index.js
+++ src/router/index.js
@@ -2,27 +2,44 @@
 
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+
+import firebase from "firebase/app";
+import "firebase/auth";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    redirect: "/login",
   },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
+  {
+    path: "/login",
+    name: "Login",
+    component: () =>
+      import(/* webpackChunkName: "login" */ "../views/Login.vue"),
+    meta: {
+      requiresAnon: true,
+    },
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  firebase.auth().onAuthStateChanged((user) => {
+    if (!user && to.matched.some((route) => route.meta.requiresAuth)) {
+      next({ path: "/login" });
+    } else if (user && to.matched.some((route) => route.meta.requiresAnon)) {
+      next({ path: "/home" });
+    } else {
+      next();
+    }
+  });
 });
 
 export default router;
