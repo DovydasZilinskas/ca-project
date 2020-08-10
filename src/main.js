171f4ef64e3c844249deb9d508013ffb396cfbff
Dovydas Zilinskas <user@Dovydas-MacBook-Air.local>
2020-08-10 18:45:50 +0300

    importuoti normalize.css, firebase ir bulma pluginai

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+/** @format */
 
-Vue.config.productionTip = false
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+
+import "./services/firebase";
+import "normalize.css";
+import "bulma/css/bulma.css";
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
