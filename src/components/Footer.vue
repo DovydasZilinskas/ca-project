ca7247055d0c8e2e86a4117ab82986f0a238f1ee
Dovydas Zilinskas <user@Dovydas-MacBook-Air.local>
2020-08-10 18:52:06 +0300

    ikeltas Footer komponentas

---
+++ src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="footeris">
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          <strong>CA-Students</strong> by
+          <a href="https://www.codeacademy.lt">Code Academy</a>.
+        </p>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style scoped>
+.footer {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  padding: 2rem 1.5rem 2rem;
+}
+</style>
