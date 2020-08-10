ec6131da270118ebea7378033537bdf33ad588c1
Dovydas Zilinskas <user@Dovydas-MacBook-Air.local>
2020-08-10 19:10:00 +0300

    Pridėtas header navigacijos komponentas

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <Header />
     <section class="section">
       <div class="container">
         <router-view />
@@ -8,3 +9,9 @@
   </div>
 </template>
 
+<script>
+import Header from "./components/Header";
+export default {
+  components: { Header },
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="header">
+    <div id="nav">
+      <nav class="navbar is-light" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+          <a class="navbar-item" target="_blank" href="https://www.codeacademy.lt">
+            <img src="https://www.codeacademy.lt/wp-content/uploads/2017/05/logo.png" />
+          </a>
+          <div class="navbar-item">
+            <h1 class="title">CA Students</h1>
+          </div>
+        </div>
+
+        <div id="navbarBasicExample" class="navbar-menu">
+          <div class="navbar-start" v-if="loggedIn">
+            <router-link
+              v-for="link in links"
+              :key="link.name"
+              :to="link.url"
+              class="navbar-item"
+            >{{link.name}}</router-link>
+          </div>
+
+          <div class="navbar-end">
+            <div class="navbar-item">
+              <div class="buttons">
+                <button
+                  v-if="loggedIn"
+                  @click="signOut"
+                  class="button is-light"
+                >Logout ({{ this.firstname }})</button>
+                <span v-else>
+                  <router-link to="/register" class="button is-dark">
+                    <strong>Sign up</strong>
+                  </router-link>
+                  <router-link to="/login" class="button is-light">Log in</router-link>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+import firebase from "firebase/app";
+import "firebase/auth";
+
+export default {
+  name: "Header",
+  data() {
+    return {
+      loggedIn: false,
+      firstname: "",
+      links: [
+        { name: "Students", url: "/" },
+        { name: "Add Student", url: "/add" },
+      ],
+    };
+  },
+  methods: {
+    signOut() {
+      this.loggedIn = false;
+      firebase.auth().signOut();
+    },
+  },
+  beforeMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.loggedIn = true;
+        this.firstname = firebase.auth().currentUser.email;
+      }
+    });
+  },
+};
+</script>
+
+<style scoped>
+</style>
